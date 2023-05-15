b0bbf7a53185bfaf3385fa8067b6df84acb932ca
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2023-05-15 14:05:31 +0200

    Added login system

---
+++ BestMovies/Entities/Model/User.cs
@@ -0,0 +1,9 @@
+namespace Entities.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int SecurityLevel { get; set; }
+    }
+}
