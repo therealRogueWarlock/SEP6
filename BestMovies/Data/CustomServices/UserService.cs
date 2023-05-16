5df323ea71e387fbaa9ab67f38d4d3672b8bd87a
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2023-05-16 11:44:11 +0200

    Update UserService.cs

--- BestMovies/Data/CustomServices/UserService.cs
+++ BestMovies/Data/CustomServices/UserService.cs
@@ -1,20 +1,23 @@
-using Entities.Models;
+using BestMovies.Models;
 
 namespace BestMovies.Data.CustomServices
 {
     public class UserService : IUserService
     {
         
+        
+        
+        
         public Task<User> ValidateUser(User user)
         {
-            if (!user.Username.Equals("admin") || !user.Password.Equals("admin")) return null;
+            if (!user.Username.Equals("admin") || !user.PasswordHash.Equals("admin")) return null;
 
             return GetAdminUser();
         }
         
         
         private Task<User> GetAdminUser() {
-            return Task.FromResult<User>(new( ) { Username = "admin", Password = "admin", SecurityLevel = 2 });
+            return Task.FromResult<User>(new( ) { Username = "admin", PasswordHash = "admin", SecurityLevel = 2 });
         }
     }
 }
