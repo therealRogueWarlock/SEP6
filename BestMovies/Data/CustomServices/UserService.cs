54f0d5ca85e0e89f164d64a797f82e761a6ebe71
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2023-05-19 15:08:13 +0200

    Added user page to nav menu, added null checks, and other updates

---
+++ BestMovies/Data/CustomServices/UserService.cs
@@ -0,0 +1,33 @@
+using System.Runtime.InteropServices;
+using BestMovies.DataAccess;
+using BestMovies.DataAccess.DataBaseAccess;
+using BestMovies.Models;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace BestMovies.Data.CustomServices
+{
+    public class UserService : IUserService
+    {
+        
+        private readonly IUserData _userData;
+        private User _currentUser;
+        
+        public UserService(IUserData userData)
+        {
+            _userData = userData;
+        }
+        
+        public async Task<User?> Validate(string username, string password)
+        {
+            _currentUser = await _userData.GetUser(username, password);
+            return _currentUser;
+        }
+        
+        public User GetCurrentUser()
+        {
+            return _currentUser;
+        }
+    }
+}
+
+
