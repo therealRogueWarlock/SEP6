7e6c2daf2e0e87198edddae186e220e1178b0bf1
FreRas <78902867+Firrez@users.noreply.github.com>
2023-05-24 14:48:57 +0200

    New Migration

--- BestMovies/Models/DbModels/Review.cs
+++ BestMovies/Models/DbModels/Review.cs
@@ -1,10 +1,15 @@
-﻿namespace BestMovies.Models.DbModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BestMovies.Models.DbModels;
 
 public class Review
 {
     public Guid Id { get; set; }
+    public string AuthorName { get; set; }
     public Guid UserId { get; set; }
-    public float Rating { get; set; }
+    
+    [Range(0, 5)]
+    public int Rating { get; set; }
     public Comment? Comment { get; set; }
     public long MovieId { get; set; }
 }
