f4094367e3a21d3781842ff6ea864a3db341ffd6
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2023-05-28 15:36:22 +0200

    Created favorite dao

---
+++ BestMovies/DataAccess/IFavoriteDao.cs
@@ -0,0 +1,12 @@
+﻿using BestMovies.Models.DbModels;
+
+namespace BestMovies.DataAccess;
+
+public interface IFavoriteDao : IDataCrud<Favourite>
+{
+    
+    public Task<List<Favourite>> GetFavoritesOfAsync(string guid);
+
+    public Task<bool> CheckIfFavorite(string userId, string subjectId);
+
+}
